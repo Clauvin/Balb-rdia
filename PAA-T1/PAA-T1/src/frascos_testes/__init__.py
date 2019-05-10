5fd0201a7c148634f9ab98708491fa80bfcbf057
Clauvin <clauvin_almeida@hotmail.com>
2019-05-10 17:26:41 -0300

    (#1) Testes feitos e AGORA SIM consertados mesmo

--- PAA-T1/PAA-T1/src/frascos_testes/__init__.py
+++ PAA-T1/PAA-T1/src/frascos_testes/__init__.py
@@ -1,5 +1,11 @@
 import frascos
 
 teste_correto = frascos.Frascos(100, 2, 53)
-result_correto = teste_correto.descobrir_onde_quebra()
-print("Resultado do teste_correto: " + str(result_1))
+resultado_correto = teste_correto.descobrir_onde_quebra()
+print("Resultado do teste_correto: " + str(resultado_correto))
+
+teste_quebrado_em_altura = frascos.Frascos(-100, 2, 53)
+
+teste_quebrado_em_garrafas = frascos.Frascos(100, -2, 53)
+
+teste_quebrado_em_onde_quebra = frascos.Frascos(100, 2, -53)
