699e9b128b57017cbb4c8ad15c5414e1f5fe6583
Clauvin <clauvin_almeida@hotmail.com>
2019-05-10 17:32:50 -0300

    (#1) Found problem with teste_correto_2

--- PAA-T1/PAA-T1/src/frascos_testes/__init__.py
+++ PAA-T1/PAA-T1/src/frascos_testes/__init__.py
@@ -9,3 +9,7 @@ teste_quebrado_em_altura = frascos.Frascos(-100, 2, 53)
 teste_quebrado_em_garrafas = frascos.Frascos(100, -2, 53)
 
 teste_quebrado_em_onde_quebra = frascos.Frascos(100, 2, -53)
+
+teste_correto_2 = frascos.Frascos(1000, 3, 245)
+resultado_correto_2 = teste_correto_2.descobrir_onde_quebra()
+print("Resultado do teste_correto_2: " + str(resultado_correto_2))
