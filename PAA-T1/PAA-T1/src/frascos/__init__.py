c9cb70fd42b1d5961fc6652e1a97e157c8787b4e
Clauvin <clauvin_almeida@hotmail.com>
2019-05-11 20:19:14 -0300

    (#2) E eu deixei alguma coisa quebrada acidentalmente

--- PAA-T1/PAA-T1/src/frascos/__init__.py
+++ PAA-T1/PAA-T1/src/frascos/__init__.py
@@ -27,7 +27,7 @@ class Frascos:
         
     def calcular_quantidade_de_pedacos(self):
         quantidade_de_pedacos = math.pow(self.altura, 1/self.garrafas)
-        #print(quantidade_de_pedacos)
+        print(quantidade_de_pedacos)
         return quantidade_de_pedacos
     
     def calcular_resto(self):
@@ -79,6 +79,6 @@ class Frascos:
     def escolher_pedaco_como_conjunto_de_alturas_de_teste(self, qual_pedaco):
         self.altura_final = self.altura_inicial + self.tamanho_de_pedaco * (qual_pedaco)
         self.altura_inicial = self.altura_inicial + self.tamanho_de_pedaco * (qual_pedaco - 1)
-        #print("Qual pedaco" + str(qual_pedaco))
-        #print("Altura inicial = " + str(self.altura_inicial))
-        #print("Altura final = " + str(self.altura_final))
+        print("Qual pedaco" + str(qual_pedaco))
+        print("Altura inicial = " + str(self.altura_inicial))
+        print("Altura final = " + str(self.altura_final))
