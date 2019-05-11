c1aa794c30f160839e587d542d5ce246978037f8
Clauvin <clauvin_almeida@hotmail.com>
2019-05-11 20:17:48 -0300

    (#2) Novos testes

--- PAA-T1/PAA-T1/src/frascos_testes/__init__.py
+++ PAA-T1/PAA-T1/src/frascos_testes/__init__.py
@@ -1,23 +1,35 @@
 import frascos
 
-#teste_correto = frascos.Frascos(100, 2, 53)
-#resultado_correto = teste_correto.descobrir_onde_quebra()
-#print("Resultado do teste_correto: " + str(resultado_correto))
+teste_correto = frascos.Frascos(100, 2, 53)
+resultado_correto = teste_correto.descobrir_onde_quebra()
+print("Resultado do teste_correto: " + str(resultado_correto))
 
-#teste_quebrado_em_altura = frascos.Frascos(-100, 2, 53)
+teste_quebrado_em_altura = frascos.Frascos(-100, 2, 53)
 
-#teste_quebrado_em_garrafas = frascos.Frascos(100, -2, 53)
+teste_quebrado_em_garrafas = frascos.Frascos(100, -2, 53)
 
-#teste_quebrado_em_onde_quebra = frascos.Frascos(100, 2, -53)
+teste_quebrado_em_onde_quebra = frascos.Frascos(100, 2, -53)
 
-#teste_correto_2 = frascos.Frascos(1000, 3, 245)
-#resultado_correto_2 = teste_correto_2.descobrir_onde_quebra()
-#print("Resultado do teste_correto_2: " + str(resultado_correto_2))
+teste_correto_2 = frascos.Frascos(1000, 3, 245)
+resultado_correto_2 = teste_correto_2.descobrir_onde_quebra()
+print("Resultado do teste_correto_2: " + str(resultado_correto_2))
 
-#teste_correto_3 = frascos.Frascos(1001, 3, 1001)
-#resultado_correto_3 = teste_correto_3.descobrir_onde_quebra()
-#print("Resultado do teste_correto_3: " + str(resultado_correto_3))
+teste_correto_3 = frascos.Frascos(1001, 3, 1001)
+resultado_correto_3 = teste_correto_3.descobrir_onde_quebra()
+print("Resultado do teste_correto_3: " + str(resultado_correto_3))
 
 teste_correto_4 = frascos.Frascos(1001, 15, 1)
 resultado_correto_4 = teste_correto_4.descobrir_onde_quebra()
 print("Resultado do teste_correto_4: " + str(resultado_correto_4))
+
+teste_correto_5 = frascos.Frascos(1001, 10, 1)
+resultado_correto_5 = teste_correto_5.descobrir_onde_quebra()
+print("Resultado do teste_correto_5: " + str(resultado_correto_5))
+
+teste_correto_6 = frascos.Frascos(1001, 8, 1)
+resultado_correto_6 = teste_correto_6.descobrir_onde_quebra()
+print("Resultado do teste_correto_6: " + str(resultado_correto_6))
+
+teste_correto_7 = frascos.Frascos(1001, 6, 1)
+resultado_correto_7 = teste_correto_7.descobrir_onde_quebra()
+print("Resultado do teste_correto_7: " + str(resultado_correto_7))
