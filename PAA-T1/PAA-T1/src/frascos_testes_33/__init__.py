4b6ae189e5c8884d70c64074a64353922605d233
Clauvin <clauvin_almeida@hotmail.com>
2019-05-18 00:47:36 -0300

    (#9) Testes melhorados

--- PAA-T1/PAA-T1/src/frascos_testes_33/__init__.py
+++ PAA-T1/PAA-T1/src/frascos_testes_33/__init__.py
@@ -5,6 +5,21 @@ teste = None
 
 if testar_conjunto:
     
-    teste = frascos.Frascos33NaoBinario(1, 100, 2, 23, 0, 0, False)
-    print(teste.iteracao_de_onde_quebra())
-    
+    for i in range(1,101):
+        
+        teste_1 = frascos.Frascos33NaoBinario(1, 100, 2, i, 0, 0, False)
+        if teste_1.iteracao_de_onde_quebra() != i:
+            print ("Erro em teste_1, altura " + str(i))
+            
+    for i in range(1,102):
+        
+        teste_2 = frascos.Frascos33NaoBinario(1, 101, 2, i, 0, 0, False)
+        if teste_2.iteracao_de_onde_quebra() != i:
+            print ("Erro em teste_2, altura " + str(i))
+            
+    for i in range(1, 54036):
+        
+        teste_3 = frascos.Frascos33NaoBinario(1, 54035, 2, i, 0, 0, False)
+        if teste_3.iteracao_de_onde_quebra() != i:
+            print ("Erro em teste_3, altura " + str(i))
+        
