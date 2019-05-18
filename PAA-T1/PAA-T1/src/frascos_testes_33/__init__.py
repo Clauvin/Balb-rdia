05845138091908b7a79cda0db97fdf7d551d0507
Clauvin <clauvin_almeida@hotmail.com>
2019-05-17 21:14:23 -0300

    (#9) Teste 1 da nova classe pronto

---
+++ PAA-T1/PAA-T1/src/frascos_testes_33/__init__.py
@@ -0,0 +1,10 @@
+import frascos
+
+testar_conjunto = True
+teste = None
+
+if testar_conjunto:
+    
+    teste = frascos.Frascos33NaoBinario(1, 100, 2, 23, 0, 0, False)
+    print(teste.iteracao_de_onde_quebra())
+    
