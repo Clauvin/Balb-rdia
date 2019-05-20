b08c2084333fde184d50902e78852b523f20a799
Clauvin <clauvin_almeida@hotmail.com>
2019-05-20 15:53:46 -0300

    (#9) Criado get_file_characters(self)

--- PAA-T1/PAA-T1/src/leitura_de_dados/__init__.py
+++ PAA-T1/PAA-T1/src/leitura_de_dados/__init__.py
@@ -9,7 +9,10 @@ class LeitorDeDadosDoPoggi:
     
     def __init__(self, file_name):
         self.file_name = file_name
-        
+    
+    def get_file_characters(self):
+        return self.file_characters
+    
     def open_file(self):
         self.file = open(self.file_name,"r")
     
