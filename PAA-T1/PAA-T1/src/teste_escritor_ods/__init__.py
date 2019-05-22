0222f03e690f03faec248ca96fe0685627ae43e3
Clauvin <clauvin_almeida@hotmail.com>
2019-05-22 18:31:22 -0300

    (#11) Adicionado teste escritor ods

---
+++ PAA-T1/PAA-T1/src/teste_escritor_ods/__init__.py
@@ -0,0 +1,23 @@
+import pyexcel_ods3
+from pyexcel_ods3 import save_data
+from collections import OrderedDict
+
+
+def save_new_sheet(name_file, sheet_name, content):
+    data = OrderedDict()
+    
+    first_line_sheet = ["Qual algoritmo", "Qual arquivo", "Qual instancia",
+                        "Quantos frascos", "Complexidade Teorica",
+                        "Tempo de CPU (ms)", "Razao"]
+    
+    final_data = []
+    final_data.append(first_line_sheet)
+    
+    for content_part in content:
+        final_data.append(content_part)
+    
+    data.update({sheet_name: final_data})
+    
+    save_data(name_file, data)
+
+save_new_sheet("test.ods", "Sheet 1", [[1, 2, 3], [4, 5, 6]])
